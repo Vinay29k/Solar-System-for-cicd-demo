00f25292f9351df216d49604e58113ee6ab153c0
Vinay Kumar <vinay29.k@gmail.com>
2026-06-29 09:54:18 +0000

    Add unit testing workflow

--- app.js
+++ app.js
@@ -11,9 +11,22 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
+// mongoose.connect(process.env.MONGO_URI, {
+//     user: process.env.MONGO_USERNAME,
+//     pass: process.env.MONGO_PASSWORD,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }, function(err) {
+//     if (err) {
+//         console.log("error!! " + err)
+//     } else {
+//       //  console.log("MongoDB Connection Successful")
+//     }
+// })
+
+mongoose.connect('mongodb://mongoadmin:secret@localhost:27017/?authSource=admin', {
+    user: 'mongoadmin',
+    pass: 'secret',
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
